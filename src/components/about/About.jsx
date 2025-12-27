9adfac9434b3ec83f1b92d8159d87e049105ec2e
Chandan Kumar <chandan@example.com>
2025-12-27 11:14:05 +0530

    4 commit

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,52 @@
 import React from 'react'
 import './about.css'
+import Me from "../../assets/me-about.jpg"
+import { FaAward } from 'react-icons/fa'
+import { FiUser } from 'react-icons/fi'
+import { VscFolderLibrary } from 'react-icons/vsc'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={Me} alt="About Image"/>
+          </div>
+        </div>
+        <div className='about__content'>
+          <div className='about__cards'>
+          <article className='about__card'>
+            <FaAward className='about__icon'/>
+            <h5>Experiance</h5>
+            <small>3+ Years Working</small>
+          </article>
+          <article className='about__card'>
+            <FiUser className='about__icon'/>
+            <h5>Clients</h5>
+            <small>200+ Worldwide</small>
+          </article>
+          <article className='about__card'>
+            <VscFolderLibrary className='about__icon'/>
+            <h5>Projects</h5>
+            <small>80+</small>
+          </article>
+       </div>
+
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Amet sequi recusandae inventore ipsa tempore, eum obcaecati iste architecto tenetur harum dolorem. Velit aperiam optio, 
+          facere repellat adipisci in molestias ullam?
+        </p>
+
+        <a href='#contact' className='btn btn-primary'>Let's Talk</a>
+         </div>
+      </div>
+    </section>
+
   )
 }
 
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './nav.css'
+import { TiHome } from "react-icons/ti";
+import { FaUser } from "react-icons/fa";
+import { FaBook } from "react-icons/fa6";
+import { RiServiceFill } from "react-icons/ri";
+import { FaMessage } from "react-icons/fa6";
 
 const Nav = () => {
+  const[activeNav,setActiveNav]=useState('#')
   return (
-    <div>Nav</div>
+    <nav>
+      <a href='#' onClick={()=> setActiveNav('#')} className={activeNav === '#' ? 'active': ''}><TiHome /></a>
+      <a href='#about' onClick={()=> setActiveNav('#about')} className={activeNav === '#about' ? 'active': ''}><FaUser /></a>
+      <a href='#experience' onClick={()=> setActiveNav('#experience')} className={activeNav === '#experience' ? 'active': ''}><FaBook /></a>
+      <a href='#services' onClick={()=> setActiveNav('#services')} className={activeNav === '#services' ? 'active': ''}>< RiServiceFill/></a>
+      <a href='#contact' onClick={()=> setActiveNav('#contact')} className={activeNav === '#contact' ? 'active': ''}><FaMessage /></a>
+    </nav>
   )
 }
 
